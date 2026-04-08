f66b42da0bc26c4dd1c5100193ab2a18ef7ffb7a
Harsh Mukund Zele <91282982+harry2611@users.noreply.github.com>
2026-04-07 18:24:19 -0700

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap <String, List<String>> map = new HashMap<>();
+        for (int i = 0; i <strs.length ; i++){
+            String s = strs[i];
+            char[] ch= s.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+
+            }
+            map.get(key).add(s);
+
+        }
+        return new ArrayList<>(map.values());
+
+    //     for (int i = 0; i < strs.length; i++) {
+    //     char[] ch = strs[i].toCharArray();
+    //     Arrays.sort(ch);
+    //     String key = String.valueOf(ch);
+    //     if (!ans.containsKey(key)) {
+    //         ans.put(key, new ArrayList<>());
+    //     }
+
+    //     ans.get(key).add(strs[i]);
+    // }
+
+    // return new ArrayList<>(ans.values());
+    }
+}
